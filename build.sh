8bcfd72b078a625316da1fc816092cfa4637286c
flowey-pc <stepanmasirov@gmail.com>
2026-01-27 21:46:07 +0500

    add bulid.sh

---
+++ build.sh
@@ -0,0 +1,8 @@
+#!/bin/sh
+set -e
+
+git clean -xdf
+
+export JAVA_HOME=/usr/lib/jvm/java-8-openjdk-amd64/
+export JAVA_TOOL_OPTIONS="-XX:+UnlockExperimentalVMOptions -XX:+AlwaysPreTouch -XX:-TieredCompilation -XX:CICompilerCount=$(nproc) -XX:ReservedCodeCacheSize=2G -XX:+DisableAttachMechanism -XX:-DontCompileHugeMethods -XX:+ParallelRefProcEnabled -XX:+PerfDisableSharedMem -XX:+UseFPUForSpilling -XX:+UseStringDeduplication -XX:+UseTransparentHugePages -XX:+UseXMMForArrayCopy -XX:+UseFastUnorderedTimeStamps"
+exec ./gradlew --no-daemon build
